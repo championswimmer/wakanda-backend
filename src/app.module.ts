7947c7cef8e99fad7020eeee89b96dbf79c14e81
Arnav Gupta <arnav@codingblocks.com>
2020-04-26 12:29:45 +0530

    add typeorm
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'blackpanther',
+      password: 'wakandaforever',
+      database: 'wakandadb',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
